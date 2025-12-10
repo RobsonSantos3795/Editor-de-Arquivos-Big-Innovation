963de45c1616beb3a65e4f2de66d4a72cbca4fc8
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2025-12-10 14:51:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function login1() {
+    window.location.href = "https://mesclar-pdf-big-innovation.vercel.app/";
+}
+function login2() {
+    window.location.href = "https://desmembrar-pdf-big-innovation.vercel.app/";
+}
+function login3() {
+    window.location.href = "https://converter-imagem-para-png-bing-inno.vercel.app/";
+}
